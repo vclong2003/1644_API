7dec32a4b38d642f44c4cbd0469855dc922e8893
vclong2003 <vclong2003@gmail.com>
2023-02-27 17:18:32 +0700

    ok

--- routes/auth.js
+++ routes/auth.js
@@ -79,7 +79,9 @@ router.post("/login", async (req, res) => {
     { expiresIn: process.env.JWT_EXPIRE }
   );
 
-  return res.cookie("token", token).sendStatus(200);
+  return res
+    .cookie("token", token, { sameSite: "none", secure: true })
+    .sendStatus(200);
 });
 
 //Logout
