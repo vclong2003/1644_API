b6488a8473dc991bb3fe5c5b5288d15eb7aa160a
vclong2003 <vclong2003@gmail.com>
2023-03-05 18:29:26 +0700

    tweak cors

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const server = express();
 const cors = require("cors");
 
 server.options("*", cors({ origin: process.env.ORIGIN, credentials: true }));
-server.use(cors({ origin: process.env.ORIGIN, credentials: true }));
+// server.use(cors({ origin: process.env.ORIGIN, credentials: true }));
 server.use(logger("dev"));
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
